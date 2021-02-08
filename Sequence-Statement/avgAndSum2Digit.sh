2eb42f967e1cfcce6f4e4b87b9afa002a4eaafd5
nikhilkumbhare <nikhilk190697@gmail.com>
2021-02-08 20:43:59 +0530

    Added : Sum And Avg of Random 2 Digit Value using While

--- Sequence-Statement/avgAndSum2Digit.sh
+++ Sequence-Statement/avgAndSum2Digit.sh
@@ -1,14 +1,14 @@
 #!/bin/bash -x
 
-count=0
-while [ $count != 6 ]
+num=0
+while [ $num != 6 ]
 do
 
 	number=$((RANDOM%100))
 	if [ $number -lt 100 ] && [ $number -gt 10 ]
 	then
 		sum=$(( $sum + $number ));
-		((count++))
+		((num++))
 	fi
 done
 
