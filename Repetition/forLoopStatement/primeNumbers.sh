ceb76765dc39de1d45c94f98ad9cdbb779b04c8e
nikhilkumbhare <nikhilk190697@gmail.com>
2021-02-08 13:09:33 +0530

    Added : To Determine Prime Numbers in Certain Number Range

---
+++ Repetition/forLoopStatement/primeNumbers.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+echo "For Prime Number In Some Range"
+read -p "Enter Start Number :" n1
+read -p "Enter End Number :" n2
+
+for (( num1=(($n1+1)); num1<$n2; num1++ ))
+do
+		number=0
+		for (( num2=1; num2<=$num1; num2++ ))
+		do
+   			if [ $(($num1%$num2)) -eq 0 ]
+   			then
+      				((number++))
+   			fi
+		done
+
+		if [ $number -eq 2 ]
+		then
+      	echo $num1
+		else
+				((number++))
+		fi
+done
