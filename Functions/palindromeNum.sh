f0af8929de9941fc0a960a4ce5cfa11983bd248d
nikhilkumbhare <nikhilk190697@gmail.com>
2021-02-09 14:21:16 +0530

    Added : To Check Random Num is Palindrome or Not

---
+++ Functions/palindromeNum.sh
@@ -0,0 +1,24 @@
+#!/bin/bash -x
+
+function palindrome(){
+	local number=$num
+	local reverse=0
+	while [ $num -gt 0 ]
+	do
+		a=`expr $num % 10 `
+		num=`expr $num / 10 `
+		reverse=`expr $reverse \* 10 + $a`
+	done
+	echo $reverse
+	if [ $number -eq $reverse ]
+	then
+    		echo "Number is palindrome"
+	else
+        	echo "Number is not palindrome"
+	fi
+}
+
+
+num=$((RANDOM))
+
+palindrome1=$( palindrome $num )
